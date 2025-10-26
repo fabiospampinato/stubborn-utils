506e097d9e7035c754e3566df116edfcdbd7edc7
Fabio Spampinato <spampinabio@gmail.com>
2025-10-26 19:32:50 +0000

    Ensuring retryify call options typs are exported too

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,10 @@ import attemptifySync from './attemptify_sync';
 import retryifyAsync from './retryify_async';
 import retryifySync from './retryify_sync';
 import type {AttemptifyAsyncOptions, AttemptifySyncOptions} from './types';
-import type {RetryifyAsyncOptions, RetryifySyncOptions} from './types';
+import type {RetryifyAsyncOptions, RetryifyAsyncCallOptions, RetryifySyncOptions, RetryifySyncCallOptions} from './types';
 
 /* EXPORT */
 
 export {attemptifyAsync, attemptifySync, retryifyAsync, retryifySync};
 export type {AttemptifyAsyncOptions, AttemptifySyncOptions};
-export type {RetryifyAsyncOptions, RetryifySyncOptions};
+export type {RetryifyAsyncOptions, RetryifyAsyncCallOptions, RetryifySyncOptions, RetryifySyncCallOptions};
